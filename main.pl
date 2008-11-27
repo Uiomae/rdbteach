817539886f6808bf634e57c01ee47a394a28e2cc
Uiomae <uiomae@gmail.com>
2008-11-27 21:11:15 +0000

    Removed commented code (unneeded)

--- main.pl
+++ main.pl
@@ -2,19 +2,4 @@
 use strict;
 use gui;
 
-#my $temp = $/;
-#undef $/;
-
-## Try to open RDB file
-#open (FILE, "$ARGV[0]") || die "Can't open '$ARGV[0]': $!\n";
-#my $rdbText = <FILE>;
-#close FILE;
-#
-## Try to open ALG file
-#open (FILE, "$ARGV[1]") || die "Can't open '$ARGV[1]': $!\n";
-#my $algText = <FILE>;
-#close FILE;
-
-#$/ = $temp;
-
 initGUI();
